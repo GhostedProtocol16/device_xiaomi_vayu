eea54e20e41cc89513c442afd1b02132c6b73957
grepfox <grepfox@tutamail.com>
2025-07-09 22:52:54 +0530

    vayu: Bringup yaap
    Change-Id: I0acfb8d436c2ebf793588d2740732804f88f06fc

--- device.mk
+++ device.mk
@@ -224,8 +224,7 @@ PRODUCT_PACKAGES += \
     ApertureOverlayVayu \
     CarrierConfigOverlayVayu \
     FrameworkResOverlayVayu \
-    LineageDialerOverlayVayu \
-    LineageSDKOverlayVayu \
+    YAAPSettingsOverlayDevice \
     SettingsOverlayVayu \
     SettingsProviderOverlayVayu \
     SystemUIOverlayVayu \
@@ -233,6 +232,9 @@ PRODUCT_PACKAGES += \
     WifiResourcesOverlayVayu
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+
+# Platform
+TARGET_BOARD_PLATFORM := msmnile
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -243,6 +245,8 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint
 
 # Power
+TARGET_PROVIDES_POWERHAL := true
+
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
@@ -309,17 +313,9 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext \
     xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext \
     xiaomi-telephony-stub
 
 # UFFD GC
